1d0f6adc24b5dfde4c1ca6af887c9c16f57e7d08
Yevgen <evgennevidim@gmail.com>
2023-01-04 20:06:53 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const TelegramApi = require('node-telegram-bot-api');
+const {gameOptions, againOptions} = require('./options')
+
+const token = '5818776413:AAE3_eZCAoi2w2iJeFgBvvlo4ZbtO_O5Ywk'
+
+const bot = new TelegramApi(token, {polling: true})
+
+const chats = {}
+
+
+const startGame = async (chatId) => {
+    await bot.sendMessage(chatId, `Сейчас я загадаю цифру от 0 до 9, а ты должен ёё угадать`)
+    const randomNumber = Math.floor(Math.random() * 10)
+    chats[chatId] = randomNumber;
+    await bot.sendMessage(chatId, 'Отгадывай', gameOptions)
+}
+
+const start = () => {
+    bot.setMyCommands([
+        {command: '/start', description: 'Начальное приветствие'},
+        {command: '/info', description: 'Получить инфо о пользователе'},
+        {command: '/game', description: 'Игра угадай цифру'}
+    ])
+
+    bot.on('message', async msg => {
+        const text = msg.text;
+        const chatId = msg.chat.id;
+
+        if (text === '/start') {
+            return bot.sendMessage(chatId, `Добро пожаловать в мой телеграм бот`)
+        }
+
+        if (text === '/info') {
+            return bot.sendMessage(chatId, `Тебя зовут ${msg.from.first_name} ${msg.from.last_name}`)
+        }
+
+        if (text === '/game') {
+            return startGame(chatId);
+        }
+        return bot.sendMessage(chatId, 'Я тебя не понимаю, попробуй еще')
+    })
+    bot.on('callback_query', async msg => {
+        const data = msg.data;
+        const chatId = msg.message.chat.id;
+        if(data === '/again') {
+            return startGame(chatId)
+        }
+        if (data === chats[chatId]) {
+            return bot.sendMessage(chatId, `Поздравляю ты угадал цифру ${chats[chatId]}`, againOptions)
+        } else {
+            return bot.sendMessage(chatId, `Ты не угадал цифру, бот загадал цифру ${chats[chatId]}`, againOptions)
+        }
+    })
+
+}
+
+start()
